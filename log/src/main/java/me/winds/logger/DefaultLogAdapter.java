0c2558e3c0a11c2a39b27882fd035bbdb7bf68c8
winds <heardown@163.com>
2021-05-07 17:31:37 +0800

    sync

--- log/src/main/java/me/winds/logger/DefaultLogAdapter.java
+++ log/src/main/java/me/winds/logger/DefaultLogAdapter.java
@@ -7,7 +7,7 @@ import android.util.Log;
 public class DefaultLogAdapter implements LogAdapter {
 
     public final static String TAG = Logger.TAG;
-    protected static final int SIZE = 3600;
+    protected static final int SIZE = 3072;
     protected static final String TOP = "\n┌─────────────────────────────";
     protected static final String BOTTOM = "\n└─────────────────────────────";
     protected static DefaultLogAdapter adapter = new DefaultLogAdapter();
@@ -25,11 +25,11 @@ public class DefaultLogAdapter implements LogAdapter {
     public void log(int priority, String tag, String extraTag, String message, String extraMessage) {
         tag = TextUtils.isEmpty(tag) ? TAG : tag;
         if (message != null && message.length() >= SIZE) {
-            Log.println(priority, tag, TextUtils.isEmpty(extraMessage) ? "" : extraMessage + TOP);
+            Log.println(priority, tag, TextUtils.isEmpty(extraMessage) ? "" : extraMessage + TOP + "\r\n");
             while (message.length() > SIZE) {// 循环分段打印日志
                 String content = message.substring(0, SIZE);
                 message = message.replace(content, "");
-                Log.println(priority, tag, TextUtils.isEmpty(extraMessage) ? "" : extraMessage + "\n" + message);
+                Log.println(priority, tag, TextUtils.isEmpty(extraMessage) ? "" : extraMessage + "\n" + content);
             }
             Log.println(priority, tag, TextUtils.isEmpty(extraMessage) ? "" : extraMessage + "\n" + message + BOTTOM);
         } else {
